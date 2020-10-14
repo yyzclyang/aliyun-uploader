21880476bba7d9d09aae469162698602e7e7fa96
yyzcl <yyzclyang@outlook.com>
2020-10-14 22:45:55 +0800

    refactor(uploader): 重构获取本地文件夹路径的方法

--- src/utils/inputUtil.ts
+++ src/utils/inputUtil.ts
@@ -7,18 +7,25 @@ export async function getRightLocalFileFolder(
   if (localFileFolder && fs.pathExistsSync(localFileFolder)) {
     return localFileFolder;
   } else {
-    return getRightLocalFileFolder(
-      await inquirer
-        .prompt({
-          type: 'input',
-          name: 'localFileFolder',
-          message: `请${
-            localFileFolder ? '正确' : ''
-          }输入需要上传的本地文件夹路径（默认为当前路径）`,
-          default: process.cwd()
-        })
-        .then(({ localFileFolder }) => localFileFolder)
-        .catch(_ => undefined)
-    );
+    localFileFolder &&
+      console.log(`本地文件夹输入错误，${localFileFolder} 不是一个有效文件夹`);
+    return inquirer
+      .prompt({
+        type: 'input',
+        name: 'localFileFolder',
+        message: `请输入需要上传的本地文件夹路径（默认为当前路径）`,
+        default: process.cwd(),
+        validate(localFileFolder: string) {
+          if (
+            !localFileFolder ||
+            (localFileFolder && fs.pathExistsSync(localFileFolder))
+          ) {
+            return true;
+          }
+          return '请输入正确的本地文件夹路径（默认为当前路径）';
+        }
+      })
+      .then(({ localFileFolder }) => localFileFolder || process.cwd())
+      .catch(_ => undefined);
   }
 }
